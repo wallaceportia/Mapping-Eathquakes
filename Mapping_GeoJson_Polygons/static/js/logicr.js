0abfa17828a735ffba581a34652ce9d74bf4e847
wallaceportia <wallaceportia@gmail.com>
2021-09-18 10:56:45 -0400

    Adding mapping data to toronto neighborhoods

--- Mapping_GeoJson_Polygons/static/js/logicr.js
+++ Mapping_GeoJson_Polygons/static/js/logicr.js
@@ -92,27 +92,22 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{
 // Then we add our 'graymap' tile layer to the map.
 //streets.addTo(map);
 // We create the dark view tile layer that will be an option for our map.
-let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
 });
 
 // Create a base layer that holds both maps.
-// let baseMaps = {
-//   Light: streets,
-//   Dark: dark
-// };
-//Create a base layer that holds both maps.
- let baseMaps = {
-  "Streets": streets,
-  "Satellite Streets": satelliteStreets
- };
+let baseMaps = {
+  Light: streets,
+  Dark: dark
+};
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
-  center: [43.7, -79.3],
-  zoom: 11,
-  layers: [streets]
+  center: [44, -80],
+  zoom: 2,
+  layers: [dark]
 });
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
@@ -120,28 +115,23 @@ function onEachFeature(feature, layer) {
   // does this feature have a property named popupContent?
     if (feature.properties ) {
       layer.bindPopup( 
-        "<b>Neighborhood: "+feature.properties.AREA_NAME+"</b>");
-        // "<b>AirLine: "+feature.properties.airline+ 
-        // "<hr>" +
-        // "Destination: "+feature.properties.dst+"</b>");
-     
+        "<b>AirLine: "+feature.properties.airline+ 
+        "<hr>" +
+        "Destination: "+feature.properties.dst+"</b>");
   }
-};
+}
 var myStyle = {
-  "color": "blue",
-  "weight": 1,
-  "fillColor": "yellow"
+  "color": "lightyellow",
+  "weight": 2
   
 };
 // Accessing the airport GeoJSON URL
 //let airportData = "https://raw.githubusercontent.com/wallaceportia/Mapping-Eathquakes/main/majorAirports.json";
 // Accessing the Toronto airline routes GeoJSON URL.
-//let torontoData = "https://raw.githubusercontent.com/wallaceportia/Mapping-Eathquakes/main/torontoRoutes.json";
-// Accessing the Toronto neighborhoods GeoJSON URL.
-let torontoHoods = "https://raw.githubusercontent.com/wallaceportia/Mapping-Eathquakes/main/torontoNeighborhoods.json"
+let torontoData = "https://raw.githubusercontent.com/wallaceportia/Mapping-Eathquakes/main/torontoRoutes.json";
 //Grabbing our GeoJSON data
 // Grabbing our GeoJSON data.
-d3.json(torontoHoods).then(function(data) {
+d3.json(torontoData).then(function(data) {
   console.log(data);
   // Creating a GeoJSON layer with the retrieved data.
   L.geoJson(data,
